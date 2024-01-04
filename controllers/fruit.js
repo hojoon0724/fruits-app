8104107b0b96b43b16b368386fa13eaefea87502
Hojoon Kim <hojoon0724@gmail.com>
2024-01-03 16:26:58 -0800

    moved routes to controller

--- controllers/fruit.js
+++ controllers/fruit.js
@@ -1,92 +1,22 @@
 // -----------------------------------------------------
+// Independent routes for different categories
+// -----------------------------------------------------
+
+// -----------------------------------------------------
 // Dependencies
 // -----------------------------------------------------
-require("dotenv").config();
-const express = require("express"); // web framework
-const morgan = require("morgan"); // logger
-const methodOverride = require("method-override"); // override forms
-
-//*not used anymore because we moved the uses to different files
-// const mongoose = require("mongoose"); // connect to our mongodb
-// const Fruit = require("./models/Fruit");
-
-const fruitController = require("./controllers/fruit");
+const express = require("express");
+const Fruit = require("../models/Fruit"); // relative location - need to go up 2 folders
 
 // -----------------------------------------------------
-// Database connection
+// Create the router
 // -----------------------------------------------------
-/* 
-?-----------------------------------------------------
-? moved to models/connection.js
-const DATABASE_URL = process.env.DATABASE_URL; // "process" is what's going on in terminal, it just exists
-
-// Establish connection
-mongoose.connect(DATABASE_URL);
-
-// Connection events that triggers depending on mongo's status
-mongoose.connection
-  .on("open", () => {
-    console.log("connected to mongo");
-  })
-  .on("close", () => {
-    console.log("disconnected from mongo");
-  })
-  .on("error", (error) => {
-    console.log(error);
-  });
-
-?----------------------------------------------------- 
-*/
+const router = express.Router();
 
 // -----------------------------------------------------
-// Create Fruits Model
+// Routes
 // -----------------------------------------------------
-/* 
-?----------------------------------------------------- 
-? moved to models/Fruit.js
-// Destructure Schema and model into their own variables
-const { Schema, model } = mongoose;
-// ^ same as
-// const Schema = mongoose.Schema
-// const model = mongoose.model
-
-// schema = the shape of the data
-const fruitSchema = new Schema({
-  name: String,
-  color: String,
-  readyToEat: Boolean,
-});
-
-// model - object for interacting with the database
-const Fruit = model("Fruit", fruitSchema);
-?----------------------------------------------------- 
-*/
-
-// -----------------------------------------------------
-// Application Object
-// -----------------------------------------------------
-const app = express();
-const { PORT = 3013 } = process.env;
-
-// -----------------------------------------------------
-// Middleware
-// -----------------------------------------------------
-app.use(morgan("dev"));
-app.use(methodOverride("_method")); // override form submissions
-app.use(express.urlencoded({ extended: true })); // allows us to parse urlencoded bodies (forms are urlencoded)
-app.use(express.static("public")); // serves files from the public folder
-app.use("/fruits", fruitController);
-//-------^ only use when the URL starts with this
-
-// -----------------------------------------------------
-// Routes INDUCESS
-// -----------------------------------------------------
-
-/*
-? -----------------------------------------------------
-? moved to controllers/fruits.js
-// Seed -- puts some data for starter info - reset the data to continue testing
-app.get("/fruits/seed", async (req, res) => {
+router.get("/seed", async (req, res) => {
   try {
     // array of all fruits
     const startFruits = [
@@ -112,7 +42,7 @@ app.get("/fruits/seed", async (req, res) => {
 });
 
 // Index
-app.get("/fruits", async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     // get all fruits
     const fruits = await Fruit.find({});
@@ -126,12 +56,12 @@ app.get("/fruits", async (req, res) => {
 });
 
 // New
-app.get("/fruits/new", (req, res) => {
+router.get("/new", (req, res) => {
   res.render("fruits/new.ejs");
 });
 
 // Delete
-app.delete("/fruits/:id", async (req, res) => {
+router.delete("/:id", async (req, res) => {
   try {
     const id = req.params.id;
     await Fruit.findByIdAndDelete(id);
@@ -143,7 +73,7 @@ app.delete("/fruits/:id", async (req, res) => {
 });
 
 // Update
-app.put("/fruits/:id", async (req, res) => {
+router.put("/:id", async (req, res) => {
   try {
     const id = req.params.id;
     req.body.readyToEat = req.body.readyToEat === "on" ? true : false;
@@ -156,7 +86,7 @@ app.put("/fruits/:id", async (req, res) => {
 });
 
 // Create
-app.post("/fruits", async (req, res) => {
+router.post("/", async (req, res) => {
   try {
     // check if readyToEat should be true
     // expression ? true : false (ternary operator)
@@ -172,7 +102,7 @@ app.post("/fruits", async (req, res) => {
 });
 
 // Edit
-app.get("/fruits/:id/edit", async (req, res) => {
+router.get("/:id/edit", async (req, res) => {
   try {
     //get the id from params
     const id = req.params.id;
@@ -189,7 +119,7 @@ app.get("/fruits/:id/edit", async (req, res) => {
 // Show - get to /fruits/:id
 // ALWAYS PUT THE SHOW ROUTE AT THE END
 // Otherwise "/new" could be taken as an ID
-app.get("/fruits/:id", async (req, res) => {
+router.get("/:id", async (req, res) => {
   try {
     // get the id from params
     const id = req.params.id;
@@ -204,19 +134,8 @@ app.get("/fruits/:id", async (req, res) => {
     res.status(400).send("yo, error. check out the log");
   }
 });
+// -----------------------------------------------------
+// Export
+// -----------------------------------------------------
 
-*/
-
-// -----------------------------------------------------
-// GET requests
-// -----------------------------------------------------
-app.get("/", (req, res) => {
-  res.send(`root response`);
-});
-
-// -----------------------------------------------------
-// Listener
-// -----------------------------------------------------
-app.listen(PORT, () => {
-  console.log(`listening in port ${PORT}`);
-});
+module.exports = router;
